6a7fa9380afa2a664aa786b78e27f72df8082b42
ayush-m1911 <ayushmetkar3@gmail.com>
2025-01-13 23:10:28 +0530

    variables learning

--- a1.js
+++ a1.js
@@ -1,1 +1,6 @@
 console.log("Hello")
+const accountID = 144553
+let accountEmail="ayush@gmail.com"
+var Password="122345"
+accountCity="Nagpur"
+console.table([accountID, accountEmail, Password, accountCity])
