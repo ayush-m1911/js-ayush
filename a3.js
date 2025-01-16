5b45e91d7c6e02b05d2965b3f1f33c1e3b669da1
ayush-m1911 <ayushmetkar3@gmail.com>
2025-01-17 00:14:35 +0530

    Datatypes Comparison done

---
+++ a3.js
@@ -0,0 +1,35 @@
+let score = "33abc"
+
+/*console.log(typeof score);*/
+
+let valueInNumber = Number(score)
+
+/*console.log( typeof valueInNumber);
+console.log(valueInNumber);*/
+
+//"33" => 33
+//"33abs"=>NaN
+//true=>1; false=>0
+
+/*let isLoggedIn = ""
+let booleamIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleamIsLoggedIn);*/
+
+// 1=> true 0=> false
+//""=>false
+//"hitesh"=> true
+
+/*let someNum=19
+let someNumber = String(someNum)
+console.log(typeof someNumber); */
+
+// ********Operations*********
+let value = 3
+let negValue = -value
+console.log(negValue);
+console.log("1" +2);
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+/*console.log(+true);*/
+
