7fe5ec79b7812f9617dea106b7d3f03ed82a213e
ayush-m1911 <ayushmetkar3@gmail.com>
2025-01-16 23:36:34 +0530

    Learning Datatypes

---
+++ a2.js
@@ -0,0 +1,11 @@
+"use strict"; //treat all JS code as newer version
+
+let name="ayush"
+let age=18
+let isLoggedIn=false
+//null is a standalone value
+//undefined means value not assigned
+//symbol=>unique
+//object
+
+console.log(typeof null) 
